e32a895fef6d365d37a475e123d38bb7aafdfae3
Rasuljonov <Rasuljonov@Muhammad>
2023-12-05 10:36:30 +0500

    fggdfx

--- src/ikitalik.ts
+++ src/ikitalik.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-const oq: HTMLParagraphElement = document.querySelector(".oq")!;
 const producktsoni: HTMLParagraphElement = document.querySelector(".producktsoni")!;
 const ong: HTMLDListElement = document.querySelector(".ong")!;
 
@@ -8,55 +7,47 @@ let responseData: any = null;
 
 const URL = "https://course-api.com/react-store-products";
 
-async function fetchData() {
-  try {
-    const response = await axios.get(URL);
-    responseData = response.data;
-    renderikitalik(responseData);
-    console.log(responseData);
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
-  }
+export  async function fetchData() {
+	try {
+		const response = await axios.get(URL);
+		responseData = response.data;
+		renderikitalik(responseData);
+		console.log(responseData);
+	} catch (error) {
+		console.error("Error fetching data:", error.message);
+	}
 }
+
+
+// fetchData();
 
 function renderikitalik(data: any) {
-  ong.innerHTML = "";
-  data.forEach((element: any, i: number) => {
-    const divelem: HTMLDivElement = document.createElement("div");
-    const img: HTMLImageElement = document.createElement("img");
-    const span: HTMLSpanElement = document.createElement("span");
-    const h4: HTMLParagraphElement = document.createElement("h4");
-    const h44: HTMLParagraphElement = document.createElement("h4");
-    const description: HTMLParagraphElement = document.createElement("p"); // Use paragraph for description
+	ong.innerHTML = "";
+	data.forEach((element: any, i: number) => {
+		const divelem: HTMLDivElement = document.createElement("div");
+		const img: HTMLImageElement = document.createElement("img");
+		const span: HTMLSpanElement = document.createElement("span");
+		const h4: HTMLParagraphElement = document.createElement("h4");
+		const h44: HTMLParagraphElement = document.createElement("h4");
+		const description: HTMLParagraphElement = document.createElement("p");
 
-    producktsoni.innerText = `${i} Products Found`;
-    description.innerText = `${element.description}`;
-    description.className="description"
-    h44.className = "summa";
-    h4.innerText = `${element.name}`;
-    h4.className = "useName";
-    h44.innerText = `$${element.price}`;
-    divelem.className = "orav";
-    img.src = `${element.image}`;
-    img.className = "img1";
-    span.className = "span1";
-    ong.className="ong1"
-    ong.appendChild(divelem);
-    divelem.appendChild(img);
-    divelem.appendChild(span);
-    span.appendChild(h4);
-    span.appendChild(h44);
-    span.appendChild(description);
-  });
+		producktsoni.innerText = `${i} Products Found`;
+		description.innerText = `${element.description}`;
+		description.className = "description";
+		h44.className = "summa";
+		h4.innerText = `${element.name}`;
+		h4.className = "useName";
+		h44.innerText = `$${element.price}`;
+		divelem.className = "orav";
+		img.src = `${element.image}`;
+		img.className = "img1";
+		span.className = "span1";
+		ong.className = "ong1";
+		ong.appendChild(divelem);
+		divelem.appendChild(img);
+		divelem.appendChild(span);
+		span.appendChild(h4);
+		span.appendChild(h44);
+		span.appendChild(description);
+	});
 }
-
-oq.addEventListener("click", () => {
-  if (responseData) {
-    renderikitalik(responseData);
-  } else {
-    fetchData();
-  }
-});
-
-
-fetchData();
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
 import "./main.css";
 import "./ikitalik";
+import { fetchData } from "./ikitalik";
 
 const ong: HTMLDListElement = document.querySelector(".ong")!;
 const category: NodeListOf<HTMLDivElement> = document.querySelectorAll("#category")!;
 const producktsoni: HTMLParagraphElement = document.querySelector(".producktsoni")!;
-const URL = "https://course-api.com/react-store-products";
+const eroe404: HTMLDivElement = document.querySelector(".conterner5")!;
+const contener: HTMLDivElement = document.querySelector(".conterner")!;
 
+const URL = "https://course-api.com/react-store-products";
+let o: any;
 async function fetchDataAndUpdate() {
 	try {
 		const response = await axios.get(URL);
@@ -14,7 +18,9 @@ async function fetchDataAndUpdate() {
 		categ(response.data);
 		console.log(response.data);
 	} catch (error) {
-		console.error("Error fetching data:", error.message);
+		eroe404.style.display = "block";
+		contener.style.display = "none";
+		console.error("Error fetching data:");
 	}
 }
 
@@ -41,36 +47,64 @@ function renderData(data: any) {
 		divelem.appendChild(span);
 		span.appendChild(h4);
 		span.appendChild(h44);
+
+		divelem.addEventListener("click", () => {
+			elemUshlash(element, divelem);
+		});
+	});
+}
+const chap: HTMLDivElement = document.querySelector(".chap")!;
+
+function elemUshlash(params: any, box: HTMLDivElement) {
+	console.log(params, box);
+	ong.innerHTML = "";
+	chap.style.display = "none";
+	ong.innerHTML = `		<div class="kattaMalum">
+<p class="nazat">BACK TO PRODUCTS</p>
+<div class="box0">
+	<img class="karimg" src="${params.image}" alt="">
+	<span class="sapn3">
+		<h2 class="ism">${params.name}</h2>
+		<h3 class="narh"> $${params.price}</h3>
+		<h3 class="parag">${params.description}</h3>
+		<h3 class="avail">Available : In Stock</h3>
+		<h3 class="sku">SKU : ${params.id}</h3>
+		<h3 class="brand">Brand : ${params.company}</h3>
+	</span>
+</div>
+</div>`;
+	const nazat: HTMLParagraphElement = document.querySelector(".nazat")!;
+
+	nazat.addEventListener("click", () => {
+		location.reload();
+
+		console.log("Nazat element clicked");
+		ong.innerHTML = "";
+		fetchDataAndUpdate();
+		chap.style.display = "flex";
 	});
 }
 
 console.log(category);
 
-function rende() {
-	for (let i = 0; i < 40; i++) {
-		const divelem: HTMLDivElement = document.createElement("div");
-		divelem.className = "loader urlImg";
-		ong.appendChild(divelem);
-	}
-}
-
 const qora: HTMLParagraphElement = document.querySelector(".qora")!;
 qora.addEventListener("click", () => {
-
-		ong.innerHTML = "";
-		fetchDataAndUpdate();
-
-});
-
-setTimeout(() => {
 	ong.innerHTML = "";
 	fetchDataAndUpdate();
-}, 4000);
+});
+
+ong.innerHTML = "";
+fetchDataAndUpdate();
 
 let a: string[] = ["office", "living room", "kitchen", "bedroom", "dining", "kids"];
 
 // kategory lar
 function categ(data: any) {
+	if (!data) {
+		console.error("data xato");
+		return;
+	}
+
 	som(data);
 	category.forEach((da: any, i) => {
 		da.addEventListener("click", () => {
@@ -80,7 +114,6 @@ function categ(data: any) {
 			const selectedCategory = da.textContent.trim().toLowerCase();
 
 			data.forEach((cat: any) => {
-
 				if (cat.category.toLowerCase() === selectedCategory || selectedCategory === "all") {
 					const divelem: HTMLDivElement = document.createElement("div");
 					const img: HTMLImageElement = document.createElement("img");
@@ -102,6 +135,9 @@ function categ(data: any) {
 					divelem.appendChild(span);
 					span.appendChild(h4);
 					span.appendChild(h44);
+					divelem.addEventListener("click", () => {
+						elemUshlash(cat, divelem);
+					});
 				}
 			});
 
@@ -116,42 +152,47 @@ function categ(data: any) {
 	});
 }
 
-const rangeInput:HTMLInputElement = document.querySelector('.vales')!;
-const pelement:HTMLParagraphElement = document.querySelector('#selectedPrice')!;
-rangeInput.addEventListener('input', amaki);
-let sega:any;
+const rangeInput: HTMLInputElement = document.querySelector(".vales")!;
+const pelement: HTMLParagraphElement = document.querySelector("#selectedPrice")!;
+rangeInput.addEventListener("input", amaki);
+let sega: any;
 
 function amaki() {
-		sega = rangeInput.value;
+	sega = rangeInput.value;
 
-		pelement.textContent = `Selected Price:  $${sega }`;
-
-	}
-	console.log(sega);
+	pelement.textContent = `Selected Price:  $${sega}`;
+}
+console.log(sega);
 
 function som(data: any) {
-	data.forEach((a: { name: any; price: any; image: any; }) => {
-			if (a.price <= sega) {
-					const divelem: HTMLDivElement = document.createElement("div");
-					const img: HTMLImageElement = document.createElement("img");
-					const span: HTMLSpanElement = document.createElement("span");
-					const h4: HTMLParagraphElement = document.createElement("h4");
-					const h44: HTMLParagraphElement = document.createElement("h4");
+	data.forEach((a: { name: any; price: any; image: any }) => {
+		if (a.price <= sega) {
+			const divelem: HTMLDivElement = document.createElement("div");
+			const img: HTMLImageElement = document.createElement("img");
+			const span: HTMLSpanElement = document.createElement("span");
+			const h4: HTMLParagraphElement = document.createElement("h4");
+			const h44: HTMLParagraphElement = document.createElement("h4");
 
-					h44.className = "h44";
-					h4.innerText = `${a.name}`;
-					h4.className = "h4";
-					h44.innerText = `$${a.price}`;
-					divelem.className = "urlImg";
-					img.src = `${a.image}`;
-					img.className = "img";
-					ong.className = "ong";
-					span.className = "span";
-					ong.appendChild(divelem);
-					divelem.appendChild(img);
-					divelem.appendChild(span);
-					span.appendChild(h4);
-					span.appendChild(h44);
-			}
+			h44.className = "h44";
+			h4.innerText = `${a.name}`;
+			h4.className = "h4";
+			h44.innerText = `$${a.price}`;
+			divelem.className = "urlImg";
+			img.src = `${a.image}`;
+			img.className = "img";
+			ong.className = "ong";
+			span.className = "span";
+			ong.appendChild(divelem);
+			divelem.appendChild(img);
+			divelem.appendChild(span);
+			span.appendChild(h4);
+			span.appendChild(h44);
+		}
 	});
 }
+
+const oq: HTMLParagraphElement = document.querySelector(".oq")!;
+
+oq.addEventListener("click", () => {
+	fetchData();
+});
